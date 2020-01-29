feb81fe54a6fe1ed4601f872489f4686ac7152d1
lucasgallardo999 <59622930+lucasgallardo999@users.noreply.github.com>
2020-01-29 17:50:51 -0300

    checkbox

--- prueba/Gallardo Lucas/Module 2/Task 2/main.js
+++ prueba/Gallardo Lucas/Module 2/Task 2/main.js
@@ -3,6 +3,7 @@
 
 const tbody = document.querySelector("tbody")
 const members = data.results[0].members
+/*
 members.forEach(member => {
     let row = tbody.insertRow (-1)
     row.innerHTML =`
@@ -12,3 +13,28 @@ members.forEach(member => {
     <td> ${member.seniority} </td>
     <td> ${member.votes_with_party_pct} </td>`
 })
+*/
+function filtrar(){
+    tbody.innerHTML=""
+    let check = document.getElementsByClassName("congress")
+    
+    for(let i=0;i<check.length;i++){
+        
+        if(check[i].checked){
+            
+            members.filter(e => e.party == check[i].value).forEach(member => {
+                
+    let row = tbody.insertRow (-1)
+    row.innerHTML =`
+    <td> <a href="${member.url}">${member.first_name} ${member.middle_name || ""} ${member.last_name} </a></td>
+    <td> ${member.party} </td>
+    <td> ${member.state} </td>
+    <td> ${member.seniority} </td>
+    <td> ${member.votes_with_party_pct} </td>`
+})
+        }
+    }
+}
+document.getElementById("rep").addEventListener("click", filtrar)
+document.getElementById("dem").addEventListener("click", filtrar)
+document.getElementById("ind").addEventListener("click", filtrar)
